b06644990b4744b77fcbef745dcf079300de1081
JΛVI <hello@soyjavi.com>
2020-09-03 07:57:31 +0700

    Scaffold

---
+++ .eslintrc.js
@@ -0,0 +1,69 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2020: true,
+    jest: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:prettier/recommended'],
+  globals: {
+    global: 'readonly',
+    require: 'readonly',
+    __DEV__: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  plugins: ['import', 'prettier', 'react', 'react-hooks', 'react-native'],
+  rules: {
+    // react
+    'react-hooks/exhaustive-deps': 'warn',
+    'react-hooks/rules-of-hooks': 'warn',
+    'react/sort-prop-types': [
+      'error',
+      {
+        callbacksLast: false,
+        ignoreCase: true,
+        requiredFirst: false,
+        sortShapeProp: true,
+      },
+    ],
+    // sorting
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal'],
+        pathGroups: [
+          {
+            pattern: '{react,react-native,reactor/**}',
+            group: 'internal',
+            position: 'before',
+          },
+          {
+            pattern: '{@**,@**/**}',
+            group: 'internal',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react', 'react-native', 'reactor'],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+    // clean-code
+    'max-len': [1, 120, { tabWidth: 2, ignoreComments: true }],
+  },
+  root: true,
+  settings: {
+    react: {
+      version: require('./package.json').dependencies.react,
+    },
+  },
+};
--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+const path = require('path');
+
+module.exports = (api) => {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          alias: {
+            '^reactor/(.+)': './src/reactor/\\1',
+
+            '@assets': './src/assets',
+            '@common': './src/common',
+            '@components': './src/components',
+            '@context': './src/context',
+            '@screens': './src/screens',
+            '@services': './src/services',
+
+            crypto: ['expo-crypto'],
+          },
+        },
+      ],
+    ],
   };
 };
